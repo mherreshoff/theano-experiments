f2b6723828338557f82d43361bad6cf94fc1bcc6
Marcello Mathias Herreshoff <marcello.herreshoff@gmail.com>
2015-06-27 15:50:31 -0700

    Added script to download MNIST data.

---
+++ fetch_mnist.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p mnist
+cd mnist
+wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+for i in *.gz; do gunzip $i; done
+

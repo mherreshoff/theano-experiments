f73518288ec0cd62ea995d691a6a61c5a84d619f
Marcello Mathias Herreshoff <marcello.herreshoff@gmail.com>
2015-06-27 18:15:16 -0700

    Cleanup.

--- perceptron_weights_to_image.py
+++ perceptron_weights_to_image.py
@@ -10,7 +10,6 @@ prog_name, h5_file_name, img_file_name = sys.argv
 h5_file = h5py.File(h5_file_name, "r")
 
 dset = h5_file["W1"]
-print dset[1:].shape
 ws = np.array(dset)[1:].transpose().reshape(-1, 28, 28)
 ws_norm = ws / np.abs(ws).max()
 
@@ -20,9 +19,6 @@ image = ws_norm.reshape(10, 10, 28, 28).swapaxes(1, 2).reshape(-1, 10*28)
 red = np.maximum(image, 0)*255.0
 green = np.zeros_like(image)
 blue = -np.minimum(image, 0)*255.0
-print red.shape
-print green.shape
-print blue.shape
 
 im = Image.fromarray(np.uint8(np.dstack((red,green,blue))))
 

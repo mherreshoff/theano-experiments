0409071b6734fd27e07b58aae8b963163c3a08d8
Marcello Mathias Herreshoff <marcello.herreshoff@gmail.com>
2015-07-02 03:02:35 -0700

    Rewrote image renderer to include margins.

--- perceptron_weights_to_image.py
+++ perceptron_weights_to_image.py
@@ -5,21 +5,26 @@ import sys
 import numpy as np
 from PIL import Image
 
+def weights_to_pic(w):
+  w_norm = w / np.abs(w).max()
+  red = np.maximum(w_norm, 0)*255.0
+  green = np.zeros_like(w_norm)
+  blue = -np.minimum(w_norm, 0)*255.0
+  return Image.fromarray(np.uint8(np.dstack((red, green, blue))))
+
+def make_grid(pics, cols=10, margin=2, bg_color="white"):
+  """aranges pics in a grid with white margins.
+  Assumes all pics are the same size."""
+  xsize, ysize = pics[0].size
+  rows = (len(pics) + cols - 1) / cols
+  im = Image.new("RGB", ((2*margin+xsize)*cols, (2*margin+ysize)*rows), bg_color)
+  for i, pic in enumerate(pics):
+    col = i % cols
+    row = i / cols
+    im.paste(pic, ((2*margin+xsize)*col+margin, (2*margin+ysize)*row+margin))
+  return im
+
 prog_name, h5_file_name, img_file_name = sys.argv
-
 h5_file = h5py.File(h5_file_name, "r")
-
-dset = h5_file["W1"]
-ws = np.array(dset).transpose().reshape(-1, 28, 28)
-ws_norm = ws / np.abs(ws).max(axis=0)
-
-# Note: assumes 10*N hidden units for now.
-image = ws_norm.reshape(-1, 10, 28, 28).swapaxes(1, 2).reshape(-1, 10*28)
-
-red = np.maximum(image, 0)*255.0
-green = np.zeros_like(image)
-blue = -np.minimum(image, 0)*255.0
-
-im = Image.fromarray(np.uint8(np.dstack((red,green,blue))))
-
-im.save(img_file_name)
+ws = np.array(h5_file["W1"]).transpose().reshape(-1, 28, 28)
+make_grid([weights_to_pic(w) for w in ws]).save(img_file_name)

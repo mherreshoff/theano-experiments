1651f984c59838491af6c5649be2099360891935
Marcello Mathias Herreshoff <marcello.herreshoff@gmail.com>
2015-07-02 20:33:02 -0700

    Added multiple plot handing to plot_error_rates.

--- plot_error_rates.py
+++ plot_error_rates.py
@@ -2,21 +2,25 @@
 import sys
 import matplotlib.pyplot as plt 
 
-f = open(sys.argv[1], "r")
-ts = []
-trains = []
-tests = []
+def read_graph(dir_name):
+  f = open(dir_name + "/graph.tsv", "r")
+  ts = []
+  trains = []
+  tests = []
 
-for line in f:
-  t, train, test = [float(x) for x in line.strip().split("\t")]
-  if train < 0.1:
-    ts.append(t)
-    trains.append(train)
-    tests.append(test)
+  for line in f:
+    t, train, test = [float(x) for x in line.strip().split("\t")]
+    if train < 0.1:
+      ts.append(t)
+      trains.append(train)
+      tests.append(test)
+  return (dir_name, ts, trains, tests)
 
+graphs = [read_graph(dir_name) for dir_name in sys.argv[1:]]
 plt.xlabel("Iteration")
 plt.ylabel("Error Rates")
-plt.plot(ts, trains, color='r', label="train")
-plt.plot(ts, tests, color='b', label="test")
+for dir_name, ts, trains, tests in graphs:
+  plt.plot(ts, trains, label=dir_name+":train")
+  plt.plot(ts, tests, label=dir_name+":test")
 plt.legend()
 plt.show()

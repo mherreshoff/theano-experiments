47004539028000a17e136314eb82fdbadf2d26b8
Marcello Mathias Herreshoff <marcello.herreshoff@gmail.com>
2015-07-02 20:24:52 -0700

    Added commandline argument handling.

--- perceptron_mnist.py
+++ perceptron_mnist.py
@@ -1,17 +1,23 @@
 #!/usr/local/bin/python
 import h5py
 import numpy
-import random
+import os
+import sys
 
 import mlp
 import mnist
 
 MAX_EPOCHS = 100
-DIMENSIONS = [28*28, 10]
+
+# Parse commandline:
+output_dir = sys.argv[1]
+dimensions = [28*28] + [int(x) for x in sys.argv[2].split("x")] + [10]
+
+os.makedirs(output_dir)
 
 # Set up the network:
 print "Setting up network..."
-net = mlp.MultiLayerPerceptron(DIMENSIONS)
+net = mlp.MultiLayerPerceptron(dimensions)
 
 print "Loading MNIST..."
 (train_x, train_y), (test_x, test_y) = mnist.load()
@@ -20,10 +26,10 @@ print "Done Loading MNIST."
 print "%d training examples" % train_x.shape[0]
 
 print "Training..."
-graph_f = open("output/graph.txt", "w")
+graph_f = open("%s/graph.tsv" % output_dir, "w")
 for i in xrange(MAX_EPOCHS+1):
   print "Ran for", i, "epochs"
-  net.write("output/epoch_%04d.hdf5" % i)
+  net.write("%s/epoch_%04d.hdf5" % (output_dir, i))
   train_error = net.error_rate(train_x, train_y)
   test_error = net.error_rate(test_x, test_y)
   graph_f.write("%d\t%f\t%f\n" % (i, train_error, test_error))
